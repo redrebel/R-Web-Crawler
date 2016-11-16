4ae0676a2ed0094a480990fde1cad02d46be73d0
Lee junggeun <cjred77@gmail.com>
2016-11-16 14:46:55 +0900

    프로토타입

--- Untitled.R
+++ Untitled.R
@@ -1,21 +1,3 @@
-library(KoNLP)
-
-useSejongDic()
-setwd("/Users/red/webhard/OneDrive/문서/대학원-MSDN-SPECIAL/논문/flog")
-data1 <- readLines("text10.txt")
-data1
-data2 <- sapply(data1, extractNoun, USE.NAMES=F)
-head(unlist(data2),30)
-data3 <- unlist(data2)
-data3 <- Filter(function(x){nchar(x)>=2}, data3)
-
-data3
-
-data3 <- gsub("\\d+", "", data3)
-data3 <- data3[data3 != ""]
-write(data3, "text11.txt")
-data4 <- read.table("text11.txt")
-data4
-wordcount <- table(data4)
-wordcount
-head(sort(wordcount, decreasing = T), 80)
+x <- data.frame(name=c("a", "b", "c"), math=c(1,2,3))
+y <- data.frame(name=c("a", "b", "d"), math=c(3,4,5))
+merge(x,y, all=TRUE)
